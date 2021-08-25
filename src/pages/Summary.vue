31b7e41bf8c59bdadd01d77146ad11348736b2f1
hyesunyun <hyesunyun09@gmail.com>
2021-08-25 18:18:00 -0400

    added prototype for diff display

--- src/pages/Summary.vue
+++ src/pages/Summary.vue
@@ -11,7 +11,19 @@
             <p>{{summary.results}}</p>
             <strong>AUTOMATIC UPDATES</strong>
             <p>{{summary.automated_narrative_summary}}</p>
-            <p>INSERT NARRATIVE SUMMARY HERE</p>
+
+            <h6>Diff</h6>
+            <p>Using diff-match-patch library</p>
+            <div id="outputdiv">
+            </div>
+
+            <div class="mt-4">
+                <p>Using v-code-diff which uses jsdiff and diff2html</p>
+                <code-diff
+                  :old-string="summary.conclusion"
+                  :new-string="summary.automated_narrative_summary"/>
+            </div>
+            
             <strong>CONCLUSIONS</strong>
             <p>{{summary.conclusion}}</p>
         </b-card>
@@ -20,19 +32,22 @@
 <script>
 import axios from 'axios';
 import settings from '../settings.js'
-
+import DiffMatchPatch from 'diff-match-patch';
+import {CodeDiff} from 'v-code-diff'
 
 export default {
     name: "Summary",
-    components: {},
+    components: {
+        CodeDiff
+    },
     data() {
         return {
             summary: {
                 bakground: null,
                 methods: null,
                 results: null,
-                conclusion: null,
-                automated_narrative_summary: null,
+                conclusion: "",
+                automated_narrative_summary: "",
             }, // summary object with all different sections
         }
     },
@@ -54,18 +69,13 @@ export default {
                 .get(`${settings.url}/api/get_live_review_summary/${this.$route.params.revid}`, { headers: headers })
                 .then(response => {
                     this.summary = response.data;
+                    const dmp = new DiffMatchPatch();
+                    const diff = dmp.diff_main(this.summary.conclusion, this.summary.automated_narrative_summary);
+                    var ds = dmp.diff_prettyHtml(diff);
+                    document.getElementById('outputdiv').innerHTML = ds;
                 }).catch(error => {
                     console.log(error); // error
                 });
-
-            // axios
-            //     .get(`${settings.url}/api/get_review_update/${this.$route.params.revid}`)
-            //     .then(response => {
-            //         this.summary = response.data;
-            //     }).catch(error => {
-            //         console.log(error); // error
-            //     });
-            // this.summary = summary;
         },
     },
     mounted() {
